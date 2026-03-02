5d6c1f165367a1faafc285e2f43120c21bca2ad3
Abraham Nkeki Thlama <abrahamnkeki@gmail.com>
2026-03-02 23:39:59 +0100

    Add Day 2 and Day 3 code

---
+++ day002/pythonDay2.py
@@ -0,0 +1,16 @@
+name = input("Enter your full name: ")
+
+print ("original text :", name)
+
+print("change text to uppercase :", name.upper())
+
+print("change text to lowaercase :", name.lower())
+
+print("remove extra space at the beginning and end of text :", name.strip())
+
+print("find length of text :", len(name))
+ 
+print("split text :", name.split())
+
+print("replaced letter a in text with b :", name.replace("a", "b"))
+
---
+++ day003/pythonDay3.py
@@ -0,0 +1,8 @@
+customers = ["Abraham", "Nkeki", "Thlama"]
+customers.append("John")
+print("Current customers:", customers)
+
+categories = ("Decorder", "DISEqC", "LNB")
+
+
+print("We sell:", ", ".join(categories))
